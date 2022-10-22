da9d07a40745c23217fc24a3053ad9b3a613436c
Vlad <azaz08082001@gmail.com>
2022-10-22 13:03:11 +0500

    card

---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="card">
+        <p>Hello</p>
+    </div>
+</template>
+<style>
+.card {
+    width: 272px;
+    height: 400px;
+    border-radius: 4px;
+    background: url(../assets/photo.png);
+    background-size: contain;
+border-radius: 4px;
+}
+</style>
