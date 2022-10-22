9e174c5635bcc67c7d62d7de98ee15b3347a883f
Vlad <azaz08082001@gmail.com>
2022-10-22 15:37:49 +0500

    card anim

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,17 @@ export default {
 </script>
 
 <style lang="scss">
+*{
+  padding: 0;
+  margin:0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Houschka Pro';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  
 }
 </style>
